3c1d69d47fd5d1558ddde23c66cada47198d2816
Adel <adelfree2023@gmail.com>
2026-01-08 21:14:30 +0200

    fix: Cast product type to fix TypeScript error

--- apps/storefront/app/[tenant]/product/[slug]/page.tsx
+++ apps/storefront/app/[tenant]/product/[slug]/page.tsx
@@ -25,7 +25,7 @@ export default async function ProductPage({
 
     return (
         <div className="container mx-auto px-4 py-8">
-            <ProductDetails product={product} />
+            <ProductDetails product={product as any} />
         </div>
     );
 }
