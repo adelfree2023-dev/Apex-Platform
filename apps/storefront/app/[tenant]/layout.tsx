35fc5f9e1a67ed6b91ea48782f9243995e772c6f
Adel <adelfree2023@gmail.com>
2026-01-09 03:00:42 +0200

    fix: Use max-w-[1440px] for wider checkout layout

--- apps/storefront/app/[tenant]/layout.tsx
+++ apps/storefront/app/[tenant]/layout.tsx
@@ -73,7 +73,7 @@ export default async function TenantLayout({
           </div>
         </header>
 
-        <main className="flex-1 px-6 py-8">
+        <main className="flex-1 w-full max-w-[1440px] mx-auto px-4 md:px-8 py-8">
           <TenantAuthGuardWrapper tenantSlug={tenant} channelToken={tenantData.vendureChannelToken || tenant}>
             {children}
           </TenantAuthGuardWrapper>
